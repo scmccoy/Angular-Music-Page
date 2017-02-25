e3638034ddee13c481055418d618e33ab11502b8
Shane <shanemccoypdx@gmail.com>
2017-02-25 18:21:46 -0500

    asdf

--- src/js/controllers/login-controller.js
+++ src/js/controllers/login-controller.js
@@ -1,28 +1,28 @@
 app.controller('LoginController', function($state, UserManagement, localStorageService) {
-	console.log('in loginController');
+	// console.log('in loginController');
 
-	// this.currentUser = null;
+	this.currentUser = null;
 	this.userInput = {};
 
 	this.userLogin = function(name, password) {
-		console.log('user', this.userInput); // logs current input
+		//console.log('user', this.userInput); // logs current input
 		// debugger;
 		userInput = {
 			name: name,
 			password: password
 		};
 		console.log('after', this.userInput); // logs current input
-		console.log('after 2 ', userInput); // logs 0 , 0
-		if (UserManagement.userCheck(this.userInput.name) === undefined || UserManagement.userCheck(this.userInput.name) === 'Guest') {
+		//console.log('after 2 ', userInput); // logs 0 , 0
+		if ((UserManagement.userCheck(this.userInput.name) === undefined) || (UserManagement.userCheck(this.userInput.name) === 'Guest')) {
 			this.currentUser.name = 'Guest';
 			console.log('currentUser in undefined --> ', this.currentUser.name);
 		} else {
-			this.currentUser = UserManagement.userCheck(this.userInput);
+			this.currentUser = UserManagement.userCheck(this.userInput.name);
 			console.log('currentUser in else --> ', this.currentUser);
 			console.log('currentUser in else --> ', this.currentUser.name);
 		}
 
-
+		// this.testStuff = userLogin(userInput.name);
 	}; // USER LOGIN FUNCTION
 
 	// this.currentUser = UserManagement.userCheck(this.userInput);
--- src/js/services/user-service.js
+++ src/js/services/user-service.js
@@ -1,11 +1,12 @@
 app.service('UserManagement', function(localStorageService) {
-	let userInput = {};
+	// let userInput = {};
 	let loginStorage = [];
 	let currentTime = Date.now();
 
 	function userCheck(userInput) {
 		console.log('in UserManagement');
-
+		// console.log('userInput --> ', userInput);
+		console.log('userinputname --> ', userInput.name);
 		let userDB = localStorageService.get('login-data');
 		if ((userDB.name === userInput.name) && (userDB.password === userInput.password)) {
 			console.log('Match');
@@ -17,8 +18,7 @@ app.service('UserManagement', function(localStorageService) {
 		} else {
 			console.log('NO MATCH!');
 			return {
-				name: 'Guest',
-				loggedIn: false
+				name: 'Guest'
 			};
 		}
 
